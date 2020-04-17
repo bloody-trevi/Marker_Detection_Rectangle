dced3a12389544f357c4dbea2b336ddc64a20310
bloody-trevi <50620289+bloody-trevi@users.noreply.github.com>
2020-04-17 15:11:43 +0900

    html theme & latex

--- source/conf.py
+++ source/conf.py
@@ -76,7 +76,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -108,22 +108,41 @@ htmlhelp_basename = 'MarkerDetectionRectangledoc'
 
 # -- Options for LaTeX output ------------------------------------------------
 
+latex_engine = 'xelatex'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    # 'papersize': 'letterpaper',
+    'papersize': 'a4paper',
 
     # The font size ('10pt', '11pt' or '12pt').
     #
-    # 'pointsize': '10pt',
+    'pointsize': '10pt',
 
     # Additional stuff for the LaTeX preamble.
     #
-    # 'preamble': '',
+    'preamble': '',
 
     # Latex figure (float) alignment
     #
-    # 'figure_align': 'htbp',
+    'figure_align': 'htbp',
+
+    # kotex config
+    'figure_align': 'htbp',
+
+    'fontpkg': r'''
+\usepackage{kotex}
+
+% 영문 폰트 설정
+setmainfont[Mapping=tex-text]{나눔고딕}
+setsansfont[Mapping=tex-text]{나눔명조}
+\setmonofont{나눔고딕코딩}
+
+% 한글 폰트 설정
+\setmainhangulfont[Mapping=tex-text]{나눔고딕}
+\setsanshangulfont[Mapping=tex-text]{나눔명조}
+\setmonohangulfont{나눔고딕코딩}
+
+''',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
